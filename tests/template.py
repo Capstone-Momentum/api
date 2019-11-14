582e32afbcc5c1e931638730037e55f6f2285412
Jonah Saya <jsaya01@gmail.com>
2019-11-14 08:59:39 -0800

    testing template

---
+++ tests/template.py
@@ -0,0 +1,42 @@
+import unittest
+from unittest.mock import patch, Mock
+import requests
+
+
+class MyTestCase(unittest.TestCase):
+
+    #mocking an API call and testing that the response is as expected
+    def test_APICall(self):
+        """Mocking a whole function"""
+        mock_get_patcher = patch('users.requests.get')
+        users = [{
+            "id": 0,
+            "first_name": "Dell",
+            "last_name": "Norval",
+            "phone": "994-979-3976"
+        }]
+
+        # Start patching 'requests.get'.
+        mock_get = mock_get_patcher.start()
+
+        # Configure the mock to return a response with status code 200 and a list of users.
+        mock_get.return_value = Mock(status_code=200)
+        mock_get.return_value.json.return_value = users
+
+        # TODO Call the service, which will send a request to the server.
+        response = requests.get("URL")
+
+        # Stop patching 'requests'.
+        mock_get_patcher.stop()
+
+        # Assert that the request-response cycle completed successfully.
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), users)
+
+
+    # basic test that can be used for testing logic
+    def test_something(self):
+        self.assertEqual(True, False)
+
+if __name__ == '__main__':
+    unittest.main()
