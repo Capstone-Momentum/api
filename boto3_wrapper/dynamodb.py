ae5abafffdde488b4407d735aec8ea5fbcc34977
Alex Tschopp <altschop@calpoly.edu>
2019-11-19 14:38:01 -0800

    typo fix

--- boto3_wrapper/dynamodb.py
+++ boto3_wrapper/dynamodb.py
@@ -3,12 +3,12 @@ import boto3
 from os import environ
 
 def get_dynamodb():
-    boto3.resource(
+    return boto3.resource(
         'dynamodb',
         aws_access_key_id='AKIA2QNHZYF3UP4HMEXZ',
         aws_secret_access_key='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',
         region_name='us-east-2'
-)
+    )
 
 def create_table(args):
     dynamodb = get_dynamodb()
